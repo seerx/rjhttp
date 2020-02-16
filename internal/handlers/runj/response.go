0dcb8b5460f7d73918641041445cc9fab1523485
dotjava <wisenow@163.com>
2020-02-16 22:34:37 +0800

    额外数据增加 tag 属性，方便客户端分辨类型

--- internal/handlers/runj/response.go
+++ internal/handlers/runj/response.go
@@ -5,10 +5,18 @@ import "github.com/seerx/runjson/pkg/rj"
 // RjResponse http 返回信息
 type RjResponse struct {
 	Error string      `json:"error,omitempty"`
-	Extra interface{} `json:"extra,omitempty"`
+	Extra *ExtraInfo  `json:"extra,omitempty"`
 	Data  rj.Response `json:"data"`
 }
 
-func (r *RjResponse) Write(data interface{}) {
-	r.Extra = data
+type ExtraInfo struct {
+	Tag  int         `json:"tag"`
+	Data interface{} `json:"data"`
+}
+
+func (r *RjResponse) Write(tag int, data interface{}) {
+	r.Extra = &ExtraInfo{
+		Tag:  tag,
+		Data: data,
+	}
 }
--- pkg/rjh/extra.go
+++ pkg/rjh/extra.go
@@ -2,5 +2,5 @@ package rjh
 
 // ExtraWriter 额外数据写入
 type ExtraWriter interface {
-	Write(data interface{})
+	Write(tag int, data interface{})
 }
